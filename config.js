9c147186982274b2332be41556a97fdd95c30df5
Stephen13211 <stephenlottering0@gmail.com>
2026-09-12 08:40:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
 
+window.PETSHOP_CONFIG = {
+  supabaseUrl: "https://jnwsehshqkrjxamtyddf.supabase.co",
+
+  supabasePublishableKey:
+    "sb_publishable_rPk1opHt26AibwT37OvGXA_1sEJ3sMH",
+
+  checkoutFunctionUrl:
+    "https://jnwsehshqkrjxamtyddf.supabase.co/functions/v1/create-checkout"
+};

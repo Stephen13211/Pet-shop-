35a7194119b63fc461dd643640858fa53fb2bf88
Stephen13211 <stephenlottering0@gmail.com>
2026-09-13 17:52:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -696,9 +696,5 @@ if (checkoutButton) {
   );
 }
 
-document.body.insertAdjacentHTML(
-  "afterbegin",
-  '<div style="position:fixed;top:0;left:0;right:0;z-index:99999;background:red;color:white;padding:15px;text-align:center;font-size:18px;">JAVASCRIPT IS WORKING</div>'
-);
 
 loadProducts();
